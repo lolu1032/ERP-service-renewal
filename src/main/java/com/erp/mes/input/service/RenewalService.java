53994264e4e11da99da86cff868dd389fa90df05
lolu1032 <soowkw@naver.com>
2025-05-02 21:24:10 +0900

    시크릿을 이용한 DB_URL 환경 변수 설정

---
+++ src/main/java/com/erp/mes/input/service/RenewalService.java
@@ -0,0 +1,9 @@
+//package com.erp.mes.input.service;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class RenewalService {
+//}
